082735d6a0da421bffc775e125a602bc7ec81601
comfort owolabi <61967699+krowniey@users.noreply.github.com>
2021-03-13 08:24:41 -0500

    first commit

---
+++ src/components/Details.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { ProductConsumer } from '../context';
+import { Link } from 'react-router-dom';
+import { ButtonContainer } from './Button';
+export default class Details extends Component {
+    render() {
+        return (
+            <ProductConsumer>
+                {value => {
+                    const { id, company, img, info, price, title, inCart } =
+                        value.detailProduct;
+                    return (
+                        <div className="container py-5">
+                            {/**title */}
+                            <div className="row">
+                                <div className="col-10 mx-auto text-center text-slanted text-green my-5">
+                                    <h1>{title}</h1>
+                                </div>
+                            </div>
+
+                            {/**end title */}
+                            {/**product info */}
+                            <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    <img src={img} className="img-fluid" alt="product" />
+                                </div>
+
+                                <div className="col-10 mx-auto col-md-6 my3 text-capitalize">
+                                    <h4>Package: {title}</h4>
+                                    <h6 className="text-title text-uppercase text-muted  mt-3 mb-2">
+                                        Packaged By :<span className="text-uppercase">
+                                            {company}
+                                        </span>
+                                    </h6>
+                                    <h6 className="text-green">
+                                        <strong>
+                                            Price: <span> # </span>{price}
+                                        </strong>
+                                    </h6>
+                                    <p className="text-capitalize  mt-3 mb-0">
+                                        Our organics:
+                                    </p>
+                                    <p className="text-muted lead">{info} </p>
+                                    {/**buttons */}
+                                    <div>
+                                        <Link to='/'>
+                                            <ButtonContainer>
+                                                back to product
+                                           </ButtonContainer>
+                                        </Link>
+                                        <Link to='/cart'>
+                                            <ButtonContainer cartColor disabled={inCart ? "yes" : "no"} onClick={() => {
+                                                value.addToCart(id);
+                                                value.openModel(id);
+
+                                            }} >
+
+                                                {inCart ? "inCart" : "add to cart"}
+                                            </ButtonContainer>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>);
+                }}
+            </ProductConsumer>
+        );
+    }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import logo from '../logo.svg';
+import styled from 'styled-components'
+import {ButtonContainer} from "./Button";
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
+                {/**https://www.iconfinder.com/icons/3316510/
+                 * cerejas_cherries_cherry_fruit_icon */}
+
+                <Link to='/' className="navbar-brand">
+                    FarMFresHorg
+                </Link>
+                <ul className="navbar-nav align-item-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            Products
+                     </Link>
+                    </li>
+                </ul>
+                <Link to='/cart' className="ml-auto">
+                <ButtonContainer>
+                    <span className="mr-2">
+                    <i className="fas fa-cart-plus" />My cart
+                    </span>
+
+                </ButtonContainer>
+                </Link>
+            </NavWrapper>
+        );
+    }
+}
+{/**styling components */}
+const NavWrapper = styled.nav`
+background: var(--mainGreen);
+.navbar-brand{
+    color: var(--mainWhite) !important;
+    font-size: 1.5rem;
+    text-transform: capitalize !important;
+}
+.nav-link{
+    color: var(--lightGreen) !important;
+    font-size: 1rem;
+
+`;
