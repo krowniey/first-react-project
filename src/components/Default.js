67863dce4b1c5d52c6f99a0f407c794c23487234
comfort owolabi <61967699+krowniey@users.noreply.github.com>
2021-03-14 10:55:27 -0400

    first commit

--- src/components/Default.js
+++ src/components/Default.js
@@ -7,7 +7,7 @@ export default class Default extends Component {
 <div className="row">
     <div className= "col-10 mx-auto text-counter text-title text-uppercase pt-5">
         <h1 className="display-3">404</h1>
-        <h1>Error</h1>
+        <h1>error</h1>
         <h2>page not found</h2>
         <h3>the requested url <span className="text-danger"> { this.props.location.pathname}</span> 
         {""}was not found</h3>
